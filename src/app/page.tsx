a79b6932affeeaea2df4e6d8236ec4ed9ffca705
rushexatrade <anggitaxs5@gmail.com>
2026-07-02 13:57:56 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import MarketExplorer from "@/components/MarketExplorer";
+import PortfolioCard from "@/components/PortfolioCard";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <MarketExplorer />
+      <PortfolioCard />
+    </>
+  );
+}
